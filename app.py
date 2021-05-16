6b575c70284eeeebc1f877c3c1595cf79329b5a5
Tejas Pradhan <pradhan.tejas135@gmail.com>
2021-05-16 19:06:28 +0530

    added oymongo code

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 import json
+import gridfs
+import base64
 import helpers.TextSummarization as ts
 import helpers.YoutubeHelper as yt
 from pymongo import MongoClient
 from flask import Flask, render_template, url_for, request, flash, redirect, jsonify, send_file, session
 from flask_cors import CORS, cross_origin
 from flask_bcrypt import Bcrypt
-#import speech_recognition as sr
+# import speech_recognition as sr
 import os
 import sys
 sys.path.insert(0, os.getcwd()+"/helpers")
-#from werkzeug.utils import secure_filename
+# from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 bcrypt = Bcrypt(app)
 client = MongoClient('localhost', 27017)
 db = client.cnt_db
 users = db.users
+fs = gridfs.GridFS(db)
 
 ALLOWED_EXTENSIONS = {'mp3', 'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
 cors = CORS(app, resources={r"/foo": {"origins": "*"}})
@@ -24,7 +27,7 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route('/')
 def index():
-    return render_template('login.html')
+    return render_template('dashboard.html')
 
 
 @app.route('/downloadfile/<text>', methods=['GET', 'POST'])
@@ -40,41 +43,58 @@ def testfn(text):
     return send_file(f, attachment_filename='minutes_of_meeting.pdf')
 
 
-@app.route('/youtube/', methods=['GET', 'POST'])
-@cross_origin(origin='*', headers=['Content-Type', 'Authorization'])
-def get_youtube_transcripts():
-    url = request.data.decode('utf-8')
-    text = yt.get_transcripts(url)
-    return text
-
-
 @app.route("/login", methods=['GET', 'POST'])
-def employee_login():
+def login():
     print(request.form['email'])
     print(request.form['password'])
     user_cred = users.find_one({'email': request.form['email']}, {
-        'email': 1, 'password': 1})
+        'name': 1, 'email': 1, 'password': 1})
     if(not bool(user_cred)):
         return render_template('signup.html')
     if(bcrypt.check_password_hash(user_cred['password'], request.form['password'])):
         # create session variable with email
         session['email'] = user_cred['email']
-        return render_template('index.html')
+        return render_template('dashboard.html', name=user_cred['name'])
     else:
         return render_template('login.html')
 
 
-@app.route("/dashboard", methods=['GET', 'POST'])
-def dashboard():
-    if 'email' not in session:
-        return render_template('login.html')
+@app.route("/generate-notes", methods=['GET', 'POST'])
+def generate_notes():
+    # if 'email' not in session:
+    #     return render_template('login.html')
+    url = request.form['url']
+    filename = request.form['name']
+    text = yt.get_transcripts(url)
+    ext_summary = ts.extractive_summary(text)
+    print("EXTRACTIVE DONE")
+    # abs_summary = ts.abstractive_summary(text)
+    # print("ABSTRACTIVE DONE")
+    ts.generate_pdf(ext_summary)
+    with open("./static/notes.pdf", "rb") as f:
+        encoded_string = base64.b64encode(f.read())
+    with fs.new_file(chunkSize=800000, filename=filename + ".pdf") as fp:
+        fp.write(encoded_string)
+    f = open("./static/notes.pdf", 'rb')
+    return send_file(f, attachment_filename='minutes_of_meeting.pdf')
 
 
 @app.route("/edit-profile", methods=['GET', 'POST'])
 def edit():
     if 'email' not in session:
         return render_template('login.html')
-    pass
+    updated_cred = users.update_one({'email': request.form['email']}, {
+        'name': request.form['name'], 'password': request.form['password'], 'number': request.form['cnum']})
+    print(updated_cred)
+
+
+@app.route("/my-pdfs/<filename>", methods=['GET', 'POST'])
+def download(filename):
+    fs = gridfs.GridFS(db)
+    # Standard query to Mongo
+    data = fs.find_one(filter=dict(filename=filename))
+    with open("./static/notes.pdf", "wb") as f:
+        f.write(base64.b64decode(data.read()))
 
 
 if __name__ == "__main__":
